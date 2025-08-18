35ea3aa53ae1f50094b99f47c79a7b179fcc0979
Sandesh Chougala <sandeshchougala205@gmail.com>
2025-08-18 19:47:49 +0530

    menu updated

--- front-end/js/course.js
+++ front-end/js/course.js
@@ -1,10 +1,74 @@
 function change() {
     const selection = document.getElementById('selection');
+    const cse1 = document.getElementById('cse1');
+    const ec1 = document.getElementById('ec1');
+
     if (selection.style.display === 'none' || selection.style.display === '') {
         selection.style.display = 'block';
-    }
-    else {
+    } else {
         selection.style.display = 'none';
+        cse1.style.display = 'none';
+        me1.style.display = 'none';
+        ec1.style.display = 'none';
+        ce1.style.display = 'none';
     }
-
 }
+
+function cse() {
+    const cse1 = document.getElementById('cse1');
+    const ec1 = document.getElementById('ec1');
+
+    if (cse1.style.display === 'none' || cse1.style.display === '') {
+        ec1.style.display = 'none';
+        ce1.style.display = 'none';
+        me1.style.display = 'none';
+        cse1.style.display = 'block';
+    } else {
+        cse1.style.display = 'none';
+    }
+}
+
+function ece() {
+    const ec1 = document.getElementById('ec1');
+    const cse1 = document.getElementById('cse1');
+
+    if (ec1.style.display === 'none' || ec1.style.display === '') {
+        cse1.style.display = 'none';
+        ce1.style.display = 'none';
+        me1.style.display = 'none';
+        ec1.style.display = 'block';
+    } else {
+        ec1.style.display = 'none';
+    }
+}
+
+function ce(){
+    const ce1 = document.getElementById('ce1');
+    const cse1 = document.getElementById('cse1');
+    const ec1 = document.getElementById('ec1');
+
+    if (ce1.style.display === 'none' || ce1.style.display === '') {
+        cse1.style.display = 'none';
+        ec1.style.display = 'none';
+        ce1.style.display = 'block';
+        me1.style.display = 'none';
+    } else {
+        ce1.style.display = 'none';
+    }
+}
+
+ function me(){
+    const me1 = document.getElementById('me1');
+    const cse1 = document.getElementById('cse1');
+    const ec1 = document.getElementById('ec1');
+    const ce1 = document.getElementById('ce1');
+
+    if (me1.style.display === 'none' || me1.style.display === '') {
+        cse1.style.display = 'none';
+        ec1.style.display = 'none';
+        ce1.style.display = 'none';
+        me1.style.display = 'block';
+    } else {
+        me1.style.display = 'none';
+    }
+ }
